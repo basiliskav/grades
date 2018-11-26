100fd3e862dd2df03f1cd2847d8e471d76be0d1a
basiliskav <basiliskavasis@gmail.com>
2018-11-26 17:28:19 +0200

    Learn about events and deligates

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -11,14 +11,36 @@ namespace Grades
         static void Main(string[] args)
         {
             GradeBook book = new GradeBook();
+
+            book.NameChanged += OnNameChanged;
+
+            book.Name = "Bill's Grade Book";
+            book.Name = "Scott's Grade Book";
+
+            book.Name = null;
             book.AddGrade(91);
             book.AddGrade(32.5f);
             book.AddGrade(75);
 
             GradeStatistics stats = book.ComputeStatistics();
-            Console.WriteLine(stats.AverageGrade);
-            Console.WriteLine(stats.HighestGrade);
-            Console.WriteLine(stats.LowestGrade);
+            Console.WriteLine(book.Name);
+            WriteResult("Average", stats.AverageGrade);
+            WriteResult("Highest", (int)stats.HighestGrade);
+            WriteResult("Lowest", stats.LowestGrade);
+        }
+        static void OnNameChanged(object sender, NameChangedEventArgs args )
+        {
+            Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}");
+        }
+
+        static void WriteResult(string description, int result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+
+        static void WriteResult(string description, float result)
+        {
+            Console.WriteLine($"{description}: {result}");
         }
     }
 }

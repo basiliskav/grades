0995e520cecefb600f35ab292aad37efb47c2602
basiliskav <basiliskavasis@gmail.com>
2018-11-26 18:02:55 +0200

    Learn about Branching (if statements) and Switch

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -12,33 +12,37 @@ namespace Grades
         {
             GradeBook book = new GradeBook();
 
-            book.NameChanged += OnNameChanged;
+            //book.NameChanged += OnNameChanged;
+            //book.Name = "Bill's Grade Book";
+            //book.Name = "Scott's Grade Book";
+            //book.Name = null;
 
-            book.Name = "Bill's Grade Book";
-            book.Name = "Scott's Grade Book";
-
-            book.Name = null;
             book.AddGrade(91);
             book.AddGrade(32.5f);
             book.AddGrade(75);
 
             GradeStatistics stats = book.ComputeStatistics();
-            Console.WriteLine(book.Name);
+            //Console.WriteLine(book.Name);
             WriteResult("Average", stats.AverageGrade);
-            WriteResult("Highest", (int)stats.HighestGrade);
+            WriteResult("Highest", stats.HighestGrade);
             WriteResult("Lowest", stats.LowestGrade);
+            WriteResult(stats.Description, stats.LetterGrade);
         }
-        static void OnNameChanged(object sender, NameChangedEventArgs args )
-        {
-            Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}");
-        }
+        //static void OnNameChanged(object sender, NameChangedEventArgs args )
+        //{
+        //    Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}");
+        //}
 
-        static void WriteResult(string description, int result)
-        {
-            Console.WriteLine(description + ": " + result);
-        }
+        //static void WriteResult(string description, int result)
+        //{
+        //    Console.WriteLine(description + ": " + result);
+        //}
 
         static void WriteResult(string description, float result)
+        {
+            Console.WriteLine($"{description}: {result}");
+        }
+        static void WriteResult(string description, string result)
         {
             Console.WriteLine($"{description}: {result}");
         }

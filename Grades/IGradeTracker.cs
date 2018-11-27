46fceed50ec4a58858b749fdda87a420f5979f79
basiliskav <basiliskavasis@gmail.com>
2018-11-27 12:49:32 +0200

    Finish C# fundamentals part 1

--- Grades/IGradeTracker.cs
+++ Grades/IGradeTracker.cs
@@ -5,10 +5,10 @@ namespace Grades
 {
     internal interface IGradeTracker :IEnumerable
     {
-        IEnumerator GetEnumerator();
         void AddGrade(float grade);
         GradeStatistics ComputeStatistics();
         void WriteGrades(TextWriter destination);
         string Name { get; set; }
+
     }
 }

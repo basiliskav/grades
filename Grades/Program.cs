cf7640b8ac815ab604581153934f3a7a380f3da8
basiliskav <basiliskavasis@gmail.com>
2018-11-27 10:46:29 +0200

    Learn about Branching, Looping, Jumping, Exceptions

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,15 +13,14 @@ namespace Grades
         {
             GradeBook book = new GradeBook();
 
-            //book.NameChanged += OnNameChanged;
-            //book.Name = "Bill's Grade Book";
-            //book.Name = "Scott's Grade Book";
-            //book.Name = null;
+            GetBookName(book);
+            AddGrades(book);
+            SaveGrades(book);
+            WriteRsults(book);
+        }
 
-            book.AddGrade(91);
-            book.AddGrade(32.5f);
-            book.AddGrade(75);
-
+        private static void WriteRsults(GradeBook book)
+        {
             GradeStatistics stats = book.ComputeStatistics();
             //Console.WriteLine(book.Name);
             WriteResult("Average", stats.AverageGrade);
@@ -28,15 +28,34 @@ namespace Grades
             WriteResult("Lowest", stats.LowestGrade);
             WriteResult(stats.Description, stats.LetterGrade);
         }
-        //static void OnNameChanged(object sender, NameChangedEventArgs args )
-        //{
-        //    Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}");
-        //}
 
-        //static void WriteResult(string description, int result)
-        //{
-        //    Console.WriteLine(description + ": " + result);
-        //}
+        private static void SaveGrades(GradeBook book)
+        {
+            using (StreamWriter outputFile = File.CreateText("grades.txt"))
+            {
+                book.WriteGrades(outputFile);
+            }
+        }
+
+        private static void AddGrades(GradeBook book)
+        {
+            book.AddGrade(91);
+            book.AddGrade(32.5f);
+            book.AddGrade(75);
+        }
+
+        private static void GetBookName(GradeBook book)
+        {
+            try
+            {
+                Console.WriteLine("Enter a Name");
+                book.Name = Console.ReadLine();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
 
         static void WriteResult(string description, float result)
         {
